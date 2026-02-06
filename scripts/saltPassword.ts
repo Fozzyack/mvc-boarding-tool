2e424e66b64590d55421df23278a540f9be78e19
Frasier <fsundra@gmail.com>
2026-02-06 17:30:55 +0800

    Adds more logs to password hash example script

--- scripts/saltPassword.ts
+++ scripts/saltPassword.ts
@@ -14,8 +14,10 @@ async function main() {
     const hash = await hashPassword();
     console.log(`Password: ${password}`);
     console.log(`Hash: ${hash}`);
-    console.log("Comparing Hash and Password ", await compareHash(hash, password));
-    console.log("Comparing Hash and Password ", await compareHash(hash, "wowee"));
+
+    console.log("Example Comparisons:")
+    console.log("Comparing Hash and Password ( gen hash:", hash, "password:", password, ")", await compareHash(hash, password));
+    console.log("Comparing Hash and Password ( gen hash:", hash, "password:", "r4ndP4ssW0rd )", await compareHash(hash, "r4ndP4ssW0rd"));
 
 }
 
